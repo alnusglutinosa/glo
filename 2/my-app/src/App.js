bd8eee4ef2ac823dc257f68cfcea87dfb6d02229
alnusglutinosa <olga.app2@gmail.com>
2021-01-31 00:39:15 +0200

    2

---
+++ 2/my-app/src/App.js
@@ -0,0 +1,21 @@
+import './source/css/style.css';
+import Header from './Header';
+import Main from './Main';
+import Features from './Features';
+import Contact from './Contact';
+import Footer from './Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+		<Main/>
+		<Features/>
+		<Contact/>
+		<Footer/>
+    </div>
+  );
+}
+
+export default App;
